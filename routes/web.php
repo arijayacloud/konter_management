a508d54e0329e4a5c4eb7a3ecb65fa495611a48e
lord <danipramoedya2002@gmail.com>
2025-05-29 18:40:32 +0700

    feat(auth): Implement redirect if authenticated

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,15 @@ Route::get('/', function () { return view('welcome'); });
 // })->name('list');
 
 use App\Http\Controllers\AuthController;
-use App\Http\Middleware\AuthCustom;
+use App\Http\Middleware\{AuthCustom, RedirectIfAuthenticatedCustom};
 
-    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->middleware(RedirectIfAuthenticatedCustom::class)->name('register.form');
+Route::post('/register', [AuthController::class, 'register'])->middleware(RedirectIfAuthenticatedCustom::class)->name('register');
 
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->middleware(RedirectIfAuthenticatedCustom::class)->name('login.form');
+Route::post('/login', [AuthController::class, 'login'])->middleware(RedirectIfAuthenticatedCustom::class)->name('login');
 
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware(AuthCustom::class)->name('logout');
 
-    // Protected route
-    // Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
-    Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware(AuthCustom::class)->name('dashboard');
-    Route::get('/list', [AuthController::class, 'list'])->middleware(AuthCustom::class)->name('list');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware(AuthCustom::class)->name('logout');
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware(AuthCustom::class)->name('dashboard');
+Route::get('/list', [AuthController::class, 'list'])->middleware(AuthCustom::class)->name('list');
